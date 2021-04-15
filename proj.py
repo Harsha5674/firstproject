1cacd466603873bec0724783f98cc4ea4f40e167
Harsha5674 <81186088+Harsha5674@users.noreply.github.com>
2021-04-15 15:08:11 +0530

    Add files via upload

---
+++ proj.py
@@ -0,0 +1,10 @@
+Python 3.9.4 (v3.9.4:1f2e3088f3, Apr  4 2021, 12:32:44) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 2+3
+5
+>>> print('MY NAME IS HARSHA')
+MY NAME IS HARSHA
+>>> print(2+3)
+5
+>>> 
